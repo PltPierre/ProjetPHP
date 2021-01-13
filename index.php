7cc7a7de0dff660f0e43254180c2dc9e4eba8936
Pierre Pairault <pairaulltpierre@gmail.com>
2021-01-13 22:47:10 +0100

    Ajout page login avec background et form

--- index.php
+++ index.php
@@ -3,9 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/index.css">
     <title>Document</title>
 </head>
 <body>
-    Hello Wolrdddd
+    <div class="bgimg"></div>
+    <div class="loginForm">
+        <form action="" class="login">
+            <label>Mail</label>
+            <br>
+            <input type="text" name="mail" id="mail" class="loginInput">
+            <br>
+            <label>Mot de passe</label>
+            <br>
+            <input type="password" name="password" id="password" class="loginInput">
+            <br>
+            <br>
+            <input type="submit" value="Valider" class="submitInput">
+        </form>
+    </div>
 </body>
 </html>
